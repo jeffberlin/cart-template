cb109c7afe8c121afd1c2dfc309ac36f43f0ceba
jeffberlin <jeff@jbrally.com>
2019-04-04 12:06:08 -0400

    Fixing

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ $("#submit").on("click", validateEmail);
 
 // adds html when the notice is needed
 function showInvalidNotice() {
-	$("#checkEmail").append('<div id="invalidEmail" class="col-xs-6 alert alert-warning alert-dismissible" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><strong>Invalid Email</strong></div>');
+	$("#checkEmail").append('<div id="invalidEmail" class="col-xs-6 alert alert-danger alert-dismissible" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><strong>Invalid Email</strong></div>');
 }
 
 // validation
